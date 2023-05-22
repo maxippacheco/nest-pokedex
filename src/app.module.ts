26ebf76e4d1ef5bee72d59085c60c0f03d5313a0
Maximo <pachecomaximo11@gmail.com>
2023-05-22 18:54:59 -0300

    environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,32 @@
 import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { ConfigService, ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
+
 import { MongooseModule } from '@nestjs/mongoose';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 
+import { EnvConfiguration } from './config/env.config';
+import { JoiValidationSchema } from './config/joi.validation';
+
 @Module({
+  
   imports: [
+    // CONFIG ENV, VA AL INICIO SIEMPRE
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema,
+    }),
+   
+    // CONFIG PUBLIC
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public')
     }),
 
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    //CONFIG DB
+    MongooseModule.forRoot(process.env.MONGODB),
 
     PokemonModule,
 
@@ -21,4 +35,5 @@ import { SeedModule } from './seed/seed.module';
     SeedModule
   ],
 })
+
 export class AppModule {}
